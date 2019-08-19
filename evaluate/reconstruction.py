5e284bd6f16c1742412048a8a3d814482a1d8099
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2019-08-19 19:40:36 +0900

    feat: implemented evaluator class that supports k-nearest neighbor search.

--- evaluate/reconstruction.py
+++ evaluate/reconstruction.py
@@ -1,19 +1,38 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-from __future__ import absolute_import
-from __future__ import unicode_literals
-from __future__ import division
-from __future__ import print_function
 
 import os, sys, io
-import argparse
+import numpy as np
 
-wd = os.path.dirname(__file__)
-wd = "." if wd == "" else wd
-os.chdir(wd)
+class NearestNeighborSearchEvaluator(object):
 
-parser = argparse.ArgumentParser(description="hoge")
-parser.add_argument("ARG_FULL", "ARG_SHORT", required=(True | False), [type = TYPE,] [default = None,] [
-    choices = LIST,] help = "MESSAGE")
-# type には __call__ method が使えるオブジェクトを指定する（例： float, int, unicode など）．
-args = parser.parse_args()
+    def __init__(self, original_representation: np.ndarray, code_representaion: np.ndarray):
+
+        assert original_representation.shape[0] == code_representaion.shape[0],\
+        "sample size must be equal."
+
+        self._mat_x = original_representation
+        self._code_x = code_representaion
+        self._n_sample = original_representation.shape[0]
+
+
+    def precision_at_k(self, sample_index: int, topk: int = 10):
+
+        assert sample_index < self._n_sample, "invalid sample index was specified."
+
+        test_code = self._code_x[sample_index,]
+        test_x = self._mat_x[sample_index,]
+
+        # top-k similarity over code representation space
+        ## remove itself
+        vec_humming_distance = np.delete(np.count_nonzero(self._code_x - test_code, axis=-1), sample_index)
+        topk_pred = np.argsort(vec_humming_distance)[:topk]
+
+        # top-k similarity over original(=dense) representation space
+        vec_l2_distance = np.delete(np.linalg.norm(self._mat_x - test_x, axis=-1), sample_index)
+        topk_gt = np.argsort(vec_l2_distance)[:topk]
+
+        # calculate precision@K
+        prec_at_k = len(set(topk_pred) & set(topk_gt)) / topk
+
+        return prec_at_k
