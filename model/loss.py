23e0ee27c1e95159d1d604a1ad80fe99c7465fb9
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2019-08-21 20:24:40 +0900

    feat: implemented reconstruction loss class: thin wrapper of MSELoss class

--- model/loss.py
+++ model/loss.py
@@ -5,10 +5,22 @@ from typing import List, Optional
 import torch
 from torch import nn
 from torch.nn import functional as F
-from torch.nn.modules.loss import _Loss
+from torch.nn.modules import loss as L
 
 
-class MutualInformationLoss(_Loss):
+class ReconstructionLoss(L._Loss):
+
+    def __init__(self):
+
+        super(ReconstructionLoss, self).__init__()
+        # sample-wise & element-wise mean
+        self._mse_loss = L.MSELoss(reduction="mean")
+
+    def forward(self, t_x_dash, t_x):
+        return self._mse_loss.forward(t_x_dash, t_x)
+
+
+class MutualInformationLoss(L._Loss):
 
     def __init__(self):
 
