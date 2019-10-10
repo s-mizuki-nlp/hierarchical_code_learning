b64cf617564bf32ac8a8b708b19af30756ce746d
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2019-10-10 23:08:41 +0900

    feat: implemented hyponymy score loss layer. all tests passed.

--- model/loss.py
+++ model/loss.py
@@ -105,45 +105,104 @@ class CodeLengthPredictionLoss(L._Loss):
 
         return loss * self._scale
 
-# class HypernymyPairwiseLoss(L._Loss):
-#
-#     def __init__(self, scale: float = 1.0, size_average=None, reduce=None, reduction='elementwise_mean'):
-#
-#         super(HypernymyPairwiseLoss, self).__init__(size_average, reduce, reduction)
-#
-#         self._scale = scale
-#
-#     def _nonzero_digits_score(self, t_prob_c_hypernym: torch.Tensor, t_prob_c_hyponym: torch.Tensor):
-#         # t_prob_c_*: (N_digits, N_ary); t_prob_c_*[n,c] = p(c_n=c)
-#         # :return: (N_digits,); return[n] = max(0, p(c_n=0|hyponym) - p(c_n=0|hypernym))
-#         return F.relu(t_prob_c_hyponym[:,0] - t_prob_c_hypernym[:,0])
-#
-#     def _nonzero_digits_overlap_score(self, t_prob_c_hypernym: torch.Tensor, t_prob_c_hyponym: torch.Tensor):
-#         # t_prob_c_*: (N_digits, N_ary); t_prob_c_*[n,c] = p(c_n=c)
-#         # :return: (N_digits,); return[n] = p(c_n!=0|hypernym) max(0, p(c_n=0|hyponym) - p(c_n=0|hypernym))
-#         return F.relu(t_prob_c_hyponym[:,0] - t_prob_c_hypernym[:,0])
-#
-#     def code_inclusion_score(self, t_prob_c_hypernym: torch.Tensor, t_prob_c_hyponym: torch.Tensor):
-#         # t_prob_c_*: (N_digits, N_ary); t_prob_c_*[n,c] = p(c_n=c)
-#         t_prob_c_nonzero_hypernym = 1.0 - t_prob_c_hypernym[:,0]
-#
-#
-#
-#     def forward(self, t_prob_c: torch.Tensor, lst_hypernymy_tuple: List[Tuple[int, int, float]]):
-#
-#         # t_prob_c: (N_b, N_digits, N_ary); t_prob_c[b,n,c] = p(c_n=c|x_b)
-#         loss = torch.scalar_tensor(0., dtype=torch.float32, requires_grad=True)
-#         if len(lst_hypernymy_tuple) == 0:
-#             return loss
-#
-#         for idx_hypernym, idx_hyponym, y_true in lst_hypernymy_tuple:
-#             prob_c_hypernym = t_prob_c[idx_hypernym,:,:]
-#             prob_c_hyponym = t_prob_c[idx_hyponym,:,:]
+
+class HyponymyScoreLoss(L._Loss):
+
+    def __init__(self, scale: float = 1.0, normalize_by_digits: bool = True, size_average=None, reduce=None, reduction='mean') -> "HyponymyScoreLoss":
+
+        super(HyponymyScoreLoss, self).__init__(size_average, reduce, reduction)
+
+        self._scale = scale
+        self._normalize_by_digits = normalize_by_digits
+        self._mse_loss = L.MSELoss(reduction=reduction)
+
+    def _dtype_and_device(self, t: torch.Tensor):
+        return t.dtype, t.device
 
 
+    def _intensity_to_probability(self, t_intensity):
+        # t_intensity can be either one or two dimensional tensor.
+        dtype, device = self._dtype_and_device(t_intensity)
+        pad_shape = t_intensity.shape[:-1] + (1,)
+
+        t_pad_begin = torch.zeros(pad_shape, dtype=dtype, device=device)
+        t_pad_end = torch.ones(pad_shape, dtype=dtype, device=device)
+
+        t_prob = torch.cumprod(1.0 - torch.cat((t_pad_begin, t_intensity), dim=-1), dim=-1) * torch.cat((t_intensity, t_pad_end), dim=-1)
+
+        return t_prob
+
+    def calc_soft_code_length(self, t_prob_c: torch.Tensor):
+        t_p_c_zero = torch.index_select(t_prob_c, dim=-1, index=torch.tensor(0)).squeeze()
+        n_digits = t_p_c_zero.shape[-1]
+        dtype, device = self._dtype_and_device(t_prob_c)
+
+        t_p_at_n = self._intensity_to_probability(t_p_c_zero)
+        t_at_n = torch.arange(n_digits+1, dtype=dtype, device=device)
+
+        ret = torch.sum(t_p_at_n * t_at_n, dim=-1)
+        return ret
+
+    def _calc_break_intensity(self, t_prob_c_x: torch.Tensor, t_prob_c_y: torch.Tensor):
+        # x: hypernym, y: hyponym
+
+        # t_p_c_*_zero: (n_batch, n_digits)
+        t_p_c_x_zero = torch.index_select(t_prob_c_x, dim=-1, index=torch.tensor(0)).squeeze()
+        t_p_c_y_zero = torch.index_select(t_prob_c_y, dim=-1, index=torch.tensor(0)).squeeze()
+
+        ret = 1.0 - t_p_c_x_zero - torch.sum(t_prob_c_x * t_prob_c_y, dim=-1) + 2 * t_p_c_x_zero * t_p_c_y_zero
+        return ret
 
 
-### experimental ###
+    def calc_soft_common_prefix_length(self, t_prob_c_x: torch.Tensor, t_prob_c_y: torch.Tensor):
+        n_digits, n_ary = t_prob_c_x.shape[-2:]
+        dtype, device = self._dtype_and_device(t_prob_c_x)
 
-class HypponyLoss(L._Loss):
-    pass
+        t_break_intensity = self._calc_break_intensity(t_prob_c_x, t_prob_c_y)
+        t_prob_break = self._intensity_to_probability(t_break_intensity)
+
+        t_at_n = torch.arange(n_digits+1, dtype=dtype, device=device)
+        ret = torch.sum(t_prob_break * t_at_n, dim=-1)
+
+        return ret
+
+    def calc_soft_hyponymy_score(self, t_prob_c_x: torch.Tensor, t_prob_c_y: torch.Tensor):
+        # calculate soft hyponymy score
+        # x: hypernym, y: hyponym
+        # t_prob_c_*[b,n,v] = Pr{C_n=v|x_b}; t_prob_c_*: (n_batch, n_digits, n_ary)
+
+        # hcl = hypernym code length
+        hcl = self.calc_soft_code_length(t_prob_c_x)
+        # cpl = common prefix length
+        cpl = self.calc_soft_common_prefix_length(t_prob_c_x, t_prob_c_y)
+
+        return cpl - hcl
+
+    def forward(self, t_prob_c_batch: torch.Tensor, lst_hyponym_tuple: List[Tuple[int,int,float]]) -> torch.Tensor:
+        """
+        evaluates L2 loss of the predicted hyponymy score and true hyponymy score.
+
+        :param t_prob_c_batch: probability array. shape: (n_batch, n_digits, n_ary), t_prob_c_batch[b,n,m] = p(c_n=m|x_b)
+        :param lst_hyponym_tuple: list of (hypernym index, hyponym index, hyponymy score) tuples
+        """
+
+        # x: hypernym, y: hyponym
+        dtype, device = self._dtype_and_device(t_prob_c_batch)
+
+        t_idx_x = torch.LongTensor([tup[0] for tup in lst_hyponym_tuple], device=device)
+        t_idx_y = torch.LongTensor([tup[1] for tup in lst_hyponym_tuple], device=device)
+        y_true = torch.FloatTensor([tup[2] for tup in lst_hyponym_tuple], device=device)
+
+        t_prob_c_x = torch.index_select(t_prob_c_batch, dim=0, index=t_idx_x)
+        t_prob_c_y = torch.index_select(t_prob_c_batch, dim=0, index=t_idx_y)
+
+        y_pred = self.calc_soft_hyponymy_score(t_prob_c_x, t_prob_c_y)
+
+        if self._normalize_by_digits:
+            n_digits = t_prob_c_batch.shape[1]
+            y_true /= n_digits
+            y_pred /= n_digits
+
+        loss = self._mse_loss(y_pred, y_true)
+
+        return loss * self._scale
