95758abcb989fccc16843475305041d0b8c89e2c
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2019-10-31 21:45:16 +0900

    feat: implemented evaluation dataset

---
+++ data/evaluation.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+from __future__ import division
+from __future__ import print_function
+
+import os, sys, io
+from typing import Dict, List, Union
+import torch
+from torch.utils.data import IterableDataset, Dataset
+import numpy as np
+import fasttext
+from gensim.models import KeyedVectors
+from wikipedia2vec import Wikipedia2Vec
+
+
+class HyponymyDataset(IterableDataset):
+
+    def __init__(self, path: str, header: bool, delimiter: str, columns: Dict[str, Union[int, slice]],
+                 description: str = "", transform=None):
+
+        super(HyponymyDataset).__init__()
+        self.path = path
+        assert os.path.exists(path), f"invalid path specified: {path}"
+
+        self._header = header
+        self._delimiter = delimiter
+        self._columns = columns
+        self._n_sample = None
+        self.description = description
+        self.transform = transform
+
+    def __len__(self):
+        if self._n_sample is not None:
+            return self._n_sample
+        else:
+            n_sample = 0
+            for _ in self:
+                n_sample += 1
+            self._n_sample = n_sample
+            return self._n_sample
+
+    def __iter__(self):
+
+        ifs = io.open(self.path, mode="r")
+        if self._header:
+            _ = next(ifs)
+
+        for s_entry in ifs:
+            lst_entry = s_entry.strip().split(self._delimiter)
+            entry = {field_name:lst_entry[field_indices] for field_name, field_indices in self._columns.items()}
+            if self.transform is not None:
+                entry = self.transform(entry)
+            yield entry
+
+        ifs.close()
+
+    @property
+    def verbose(self):
+        ret = {
+            "path": self.path,
+            "n_sample": self.__len__(),
+            "description": self.description,
+            "transform": self.transform
+        }
+        return ret
--- data/transform.py
+++ data/transform.py
@@ -18,3 +18,20 @@ class EmbeddingNormalizer(object):
         sample["embedding"] = x / np.linalg.norm(x)
 
         return sample
+
+
+class HyponymyEntryToListOfHyponymyPair(object):
+
+    def __call__(self, sample):
+        hyponym = sample.get("hyponym", None)
+        hypernym = sample.get("hypernym", None)
+        hypernyms = sample.get("hypernyms", None)
+
+        assert hyponym is not None, "could not get hyponym."
+
+        if hypernym is not None:
+            return [(hyponym, hypernym)]
+        elif isinstance(hypernyms, list):
+            return [(hyponym, hyper) for hyper in hypernyms]
+        else:
+            raise AssertionError("could not get hypernym.")
