bcfd69ced8cba1d1c8c8b46857f195da82fa2ded
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2019-11-16 17:11:30 +0900

    refactor: refacoring in progress so that either integer or entity name can be used as a item key.

--- data/word_embeddings.py
+++ data/word_embeddings.py
@@ -1,37 +1,68 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-import sys, io, os
-from typing import Union
+import io, os
+from typing import Union, Collection
+
+from abc import ABCMeta, abstractmethod
 
 from nltk.tokenize import MWETokenizer
 import torch
-from torch.utils.data import IterableDataset, Dataset
+from torch.utils.data import Dataset
 import numpy as np
 import fasttext
 from gensim.models import KeyedVectors
-from wikipedia2vec import Wikipedia2Vec
 
-class ToyEmbeddingsDataset(Dataset):
 
-    def __init__(self, sample_size: int, embedding_dim: int, transform=None, seed: int = 0):
+class AbstractWordEmbeddingsDataset(Dataset, metaclass=ABCMeta):
+    _PHRASE_DELIMITER = "_"
+    _idx_to_word = {}
+    transform = None
 
-        np.random.seed(seed)
-        self.embedding = np.random.normal(size=sample_size * embedding_dim).astype(np.float32).reshape((sample_size, embedding_dim))
-        self._idx_to_word = {idx:f"{idx}" for idx in range(sample_size)}
-        self.transform = transform
-        self._n_sample = sample_size
+    def phrase_splitter(self, phrase: str):
+        return self._mwe_tokenizer.tokenize(phrase.split(self._PHRASE_DELIMITER))
 
-    def __len__(self):
-        return self._n_sample
+    def _init_mwe_tokenizer(self):
+        def multi_word_expressions():
+            for entity in self.vocab:
+                if entity.find(self._PHRASE_DELIMITER) != -1:
+                    yield entity.split(self._PHRASE_DELIMITER)
+        it = multi_word_expressions()
+        self._mwe_tokenizer = MWETokenizer(it)
 
-    def __getitem__(self, idx):
+    @abstractmethod
+    def encode_phrase(self, phrase):
+        pass
 
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
+    @abstractmethod
+    def encode(self, entity):
+        pass
 
-        word = self._idx_to_word[idx]
-        embedding = self.embedding[idx,:]
+    def is_encodable(self, entity: str):
+        vec_e = self.encode(entity)
+        return vec_e is not None
+
+    @abstractmethod
+    def vocab(self) -> Collection[str]:
+        pass
+
+    @abstractmethod
+    def n_dim(self):
+        pass
+
+    def __getitem__(self, key: Union[torch.Tensor, int, str]):
+        if torch.is_tensor(key):
+            key = key.tolist()
+
+        if isinstance(key, int):
+            word = self._idx_to_word[key]
+        elif isinstance(key, str):
+            word = key
+        else:
+            raise NotImplementedError(f"unsupprted key type: {type(key)}")
+
+        embedding = self.encode(word)
+        assert embedding is not None, f"string {word} cannot be encoded."
 
         sample = {"entity":word, "embedding":embedding}
 
@@ -40,22 +71,21 @@ class ToyEmbeddingsDataset(Dataset):
 
         return sample
 
-    @property
-    def n_dim(self):
-        return self.embedding.shape[1]
 
+class GeneralPurposeEmbeddingsDataset(AbstractWordEmbeddingsDataset):
 
-class GeneralPurposeEmbeddingsDataset(Dataset):
-
-    def __init__(self, path_numpy_array_binary_format: str, path_vocabulary_text: str, transform=None):
+    def __init__(self, path_numpy_array_binary_format: str, path_vocabulary_text: str,
+                 enable_phrase_composition:bool = False, transform=None):
 
         self.embedding = np.load(path_numpy_array_binary_format)
         sample_size = self.embedding.shape[0]
         self._vocabulary = self._load_vocabulary_text(path_vocabulary_text)
         assert len(self._vocabulary) == sample_size, "embeddings and vocabulary size mismatch detected."
         self._idx_to_word = {idx:word for idx, word in enumerate(self._vocabulary)}
+        self._entity_to_idx = {word:idx for idx, word in enumerate(self._vocabulary)}
         self.transform = transform
         self._n_sample = sample_size
+        self._enable_phrase_composition = enable_phrase_composition
 
     def _load_vocabulary_text(self, path_vocabulary_text: str):
         lst_v = []
@@ -67,24 +97,29 @@ class GeneralPurposeEmbeddingsDataset(Dataset):
     def __len__(self):
         return self._n_sample
 
-    def __getitem__(self, idx):
+    def encode(self, entity):
+        if entity in self.vocab:
+            return self.embedding[self._entity_to_idx[entity], :]
+        else:
+            if self._enable_phrase_composition:
+                return self.encode_phrase(entity)
+            else:
+                return None
 
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-
-        word = self._idx_to_word[idx]
-        embedding = self.embedding[idx,:]
-
-        sample = {"entity":word, "embedding":embedding}
-
-        if self.transform is not None:
-            sample = self.transform(sample)
-
-        return sample
+    def encode_phrase(self, phrase):
+        lst_tokens = self.phrase_splitter(phrase)
+        if not all([token in self.vocab for token in lst_tokens]):
+            return None
+        vec_r = np.mean(np.stack([self.embedding[self._entity_to_idx[token],:] for token in lst_tokens]), axis=0)
+        return vec_r
 
     @property
     def n_dim(self):
         return self.embedding.shape[1]
+
+    @property
+    def vocab(self):
+        return self._entity_to_idx
 
 
 class FastTextDataset(Dataset):
@@ -120,9 +155,7 @@ class FastTextDataset(Dataset):
         return self.model.get_dimension()
 
 
-class Word2VecDataset(Dataset):
-
-    _PHRASE_DELIMITER = "_"
+class Word2VecDataset(AbstractWordEmbeddingsDataset):
 
     def __init__(self, path_word2vec_format: str, binary: bool = True, init_sims: bool = False, transform=None,
                  enable_phrase_composition=True, **kwargs):
@@ -139,39 +172,8 @@ class Word2VecDataset(Dataset):
         if enable_phrase_composition:
             self._init_mwe_tokenizer()
 
-    def _init_mwe_tokenizer(self):
-        def multi_word_expressions():
-            for entity in self.vocab:
-                if entity.find(self._PHRASE_DELIMITER) != -1:
-                    yield entity.split(self._PHRASE_DELIMITER)
-        it = multi_word_expressions()
-        self._mwe_tokenizer = MWETokenizer(it)
-
     def __len__(self):
         return self._n_sample
-
-    def __getitem__(self, key: Union[torch.Tensor, int, str]):
-
-        if torch.is_tensor(key):
-            key = key.tolist()
-
-        if isinstance(key, int):
-            word = self._idx_to_word[key]
-            embedding = self.model.get_vector(word)
-        elif isinstance(key, str):
-            word = key
-            embedding = self.encode(word)
-            assert embedding is not None, f"string {word} cannot be encoded."
-
-        sample = {"entity":word, "embedding":embedding}
-
-        if self.transform is not None:
-            sample = self.transform(sample)
-
-        return sample
-
-    def phrase_splitter(self, phrase: str):
-        return self._mwe_tokenizer.tokenize(phrase.split(self._PHRASE_DELIMITER))
 
     def encode_phrase(self, phrase: str):
         lst_tokens = self.phrase_splitter(phrase)
@@ -189,10 +191,6 @@ class Word2VecDataset(Dataset):
             else:
                 return None
 
-    def is_encodable(self, entity: str):
-        vec_e = self.encode(entity)
-        return vec_e is not None
-
     @property
     def n_dim(self):
         return self.model.vector_size
@@ -200,37 +198,3 @@ class Word2VecDataset(Dataset):
     @property
     def vocab(self):
         return self.model.vocab
-
-
-class Wikipedia2VecDataset(Dataset):
-
-    def __init__(self, path_wikipedia2vec: str, transform=None):
-        warnings.warn("experimental dataset.")
-
-        assert os.path.exists(path_wikipedia2vec), f"file not found: {path_wikipedia2vec}"
-        self.model = Wikipedia2Vec.load(path_wikipedia2vec)
-        self.transform = transform
-        self._idx_to_entity = {idx:entity for idx, entity in enumerate(self.model.dictionary.entities())}
-        self._n_sample = len(self._idx_to_entity)
-
-    def __len__(self):
-        return self._n_sample
-
-    def __getitem__(self, idx):
-
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-
-        entity = self._idx_to_entity[idx]
-        embedding = self.model.get_vector(entity)
-
-        sample = {"entity":entity.title, "embedding":embedding}
-
-        if self.transform is not None:
-            sample = self.transform(sample)
-
-        return sample
-
-    @property
-    def n_dim(self):
-        return self.model.train_params["dim_size"]
