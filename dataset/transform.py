464605e1fc9590d6e993839992bed6627acc7349
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2019-11-16 21:48:53 +0900

    feat: added transformation class

--- dataset/transform.py
+++ dataset/transform.py
@@ -5,27 +5,34 @@ from __future__ import unicode_literals
 from __future__ import division
 from __future__ import print_function
 
+from typing import Dict, Callable
 import os, sys, io
-import argparse
-
 import numpy as np
 
 class EmbeddingNormalizer(object):
 
+    def __init__(self, field_name_embedding="embedding"):
+        self._field_name = field_name_embedding
+
     def __call__(self, sample):
 
-        x = sample["embedding"]
-        sample["embedding"] = x / np.linalg.norm(x)
+        x = sample[self._field_name]
+        sample[self._field_name] = x / np.linalg.norm(x)
 
         return sample
 
 
 class HyponymyEntryToListOfHyponymyPair(object):
 
+    def __init__(self, field_name_hyponym: str = "hyponym", field_name_hypernym: str = "hypernym", field_name_hypernyms: str = "hypernyms"):
+        self._field_name_hyponym = field_name_hyponym
+        self._field_name_hypernym = field_name_hypernym
+        self._field_name_hypernyms = field_name_hypernyms
+
     def __call__(self, sample):
-        hyponym = sample.get("hyponym", None)
-        hypernym = sample.get("hypernym", None)
-        hypernyms = sample.get("hypernyms", None)
+        hyponym = sample.get(self._field_name_hyponym, None)
+        hypernym = sample.get(self._field_name_hypernym, None)
+        hypernyms = sample.get(self._field_name_hypernyms, None)
 
         assert hyponym is not None, "could not get hyponym."
 
@@ -35,3 +42,18 @@ class HyponymyEntryToListOfHyponymyPair(object):
             return [(hyponym, hyper) for hyper in hypernyms]
         else:
             raise AssertionError("could not get hypernym.")
+
+
+class FieldTypeConverter(object):
+
+    def __init__(self, dict_field_type_converter: Dict[str, Callable]):
+
+        self._dict_field_type_converter = dict_field_type_converter
+
+    def __call__(self, sample):
+
+        for field_name, converter in self._dict_field_type_converter.items():
+            if field_name in sample:
+                sample[field_name] = converter(sample[field_name])
+
+        return sample
