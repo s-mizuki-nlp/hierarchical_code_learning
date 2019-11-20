7fe41cd9d8bf836fb01355cd532b10f77745c644
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2019-11-20 22:50:11 +0900

    feat: implemented trainer class for supervised learning

--- lightning_module/trainer.py
+++ lightning_module/trainer.py
@@ -16,15 +16,15 @@ from torch.nn.modules.loss import _Loss
 from torch.optim import Adam
 import pytorch_lightning as pl
 
-from model.autoencoder import AutoEncoder
-from model.encoder import SimpleEncoder, CodeLengthAwareEncoder
+from model.autoencoder import AutoEncoder, MaskedAutoEncoder
+from model.loss import ReconstructionLoss, HyponymyScoreLoss
 
 class UnsupervisedTrainer(pl.LightningModule):
 
     def __init__(self,
-                 model: AutoEncoder, loss_reconst: _Loss,
+                 model: AutoEncoder,
+                 loss_reconst: _Loss,
                  loss_mutual_info: Optional[_Loss] = None,
-                 coef_loss_mutual_info: Optional[float] = 1.0,
                  dataloader_train: Optional[DataLoader] = None,
                  dataloader_val: Optional[DataLoader] = None,
                  dataloader_test: Optional[DataLoader] = None,
@@ -38,13 +38,14 @@ class UnsupervisedTrainer(pl.LightningModule):
         self._decoder = model._decoder
         self._loss_reconst = loss_reconst
         self._loss_mutual_info = loss_mutual_info
-        self._coef_loss_mutual_info = coef_loss_mutual_info
         self._learning_rate = learning_rate
         self._dataloaders = {
             "train": dataloader_train,
             "val": dataloader_val,
             "test": dataloader_test
         }
+        # auxiliary function that is solely used for validation
+        self._auxiliary = HyponymyScoreLoss()
 
     def _numpy_to_tensor(self, np_array: np.array):
         return torch.from_numpy(np_array).to(self._device)
@@ -93,15 +94,16 @@ class UnsupervisedTrainer(pl.LightningModule):
     def _evaluate_code_stats(self, t_code_prob):
 
         n_ary = self._model.n_ary
-        effective_code_length = torch.sum(1.0 - t_code_prob[:,:,0], axis=-1)
-        code_divergence = torch.mean(np.log(n_ary) + torch.sum(t_code_prob * torch.log(t_code_prob), axis=-1), axis=-1)
+        soft_code_length = self._auxiliary.calc_soft_code_length(t_code_prob)
+        code_probability_divergence = torch.mean(np.log(n_ary) + torch.sum(t_code_prob * torch.log(t_code_prob), axis=-1), axis=-1)
 
         metrics = {
-            "val_effective_code_length_mean":torch.mean(effective_code_length),
-            "val_effective_code_length_std":torch.std(effective_code_length),
-            "val_code_divergence":torch.mean(code_divergence)
+            "val_soft_code_length_mean":torch.mean(soft_code_length),
+            "val_soft_code_length_std":torch.std(soft_code_length),
+            "val_code_probability_divergence":torch.mean(code_probability_divergence)
         }
         return metrics
+
 
     def validation_step(self, data_batch, batch_nb):
 
@@ -115,7 +117,7 @@ class UnsupervisedTrainer(pl.LightningModule):
         else:
             loss_mi = torch.tensor(0.0, dtype=torch.float32)
 
-        loss = loss_reconst + self._coef_loss_mutual_info * loss_mi
+        loss = loss_reconst + loss_mi
 
         metrics = {
             "val_loss_reconst": loss_reconst,
@@ -160,8 +162,75 @@ class UnsupervisedTrainer(pl.LightningModule):
         return model
 
 
-class SupervisedCodeLengthTrainer(UnsupervisedTrainer):
-    pass
-
 class SupervisedHypernymyRelationTrainer(UnsupervisedTrainer):
+
+    def __init__(self,
+                 model: MaskedAutoEncoder,
+                 loss_reconst: ReconstructionLoss,
+                 loss_hyponymy: HyponymyScoreLoss,
+                 loss_mutual_info: Optional[_Loss] = None,
+                 dataloader_train: Optional[DataLoader] = None,
+                 dataloader_val: Optional[DataLoader] = None,
+                 dataloader_test: Optional[DataLoader] = None,
+                 learning_rate: Optional[float] = 0.001
+                 ):
+
+        super(SupervisedHypernymyRelationTrainer, self).__init__(model, loss_reconst, loss_mutual_info, dataloader_train, dataloader_val, dataloader_test, learning_rate)
+
+        self._loss_hyponymy = loss_hyponymy
+
+    def training_step(self, data_batch, batch_nb):
+
+        # forward computation
+        t_x = data_batch["embedding"]
+        lst_tup_hyponymy = data_batch["hyponymy_relation"]
+        t_latent_code, t_code_prob, t_x_dash = self._model.forward(t_x)
+
+        loss_reconst = self._loss_reconst.forward(t_x_dash, t_x)
+        loss_hyponymy = self._loss_hyponymy(t_code_prob, lst_tup_hyponymy)
+
+        if self._loss_mutual_info is not None:
+            loss_mi = self._loss_mutual_info(t_code_prob)
+        else:
+            loss_mi = torch.tensor(0.0, dtype=torch.float32)
+
+        loss = loss_reconst + loss_hyponymy + loss_mi
+
+        dict_losses = {
+            "loss_reconst": float(loss_reconst),
+            "loss_mutual_info": float(loss_mi),
+            "loss_hyponymy": float(loss_hyponymy),
+            "loss": loss
+        }
+        return dict_losses
+
+    def validation_step(self, data_batch, batch_nb):
+
+        # forward computation without back-propagation
+        t_x = data_batch["embedding"]
+        lst_tup_hyponymy = data_batch["hyponymy_relation"]
+        t_intermediate, t_code_prob, t_x_dash = self._model._predict(t_x)
+
+        loss_reconst = self._loss_reconst(t_x_dash, t_x)
+        loss_hyponymy = self._loss_hyponymy(t_code_prob, lst_tup_hyponymy)
+        if self._loss_mutual_info is not None:
+            loss_mi = self._loss_mutual_info(t_code_prob)
+        else:
+            loss_mi = torch.tensor(0.0, dtype=torch.float32)
+
+        loss = loss_reconst + loss_hyponymy + loss_mi
+
+        metrics = {
+            "val_loss_reconst": loss_reconst,
+            "val_loss_mutual_info": loss_mi,
+            "val_loss_hyponymy": loss_hyponymy,
+            "val_loss": loss
+        }
+        metrics_repr = self._evaluate_code_stats(t_code_prob)
+        metrics.update(metrics_repr)
+
+        return metrics
+
+
+class SupervisedCodeLengthTrainer(UnsupervisedTrainer):
     pass
