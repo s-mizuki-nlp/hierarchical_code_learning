f4b67614f4b8314a21780c78d889d395ae5cac7d
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2019-11-23 22:27:46 +0900

    ci: added encow16ax word2vec embeddings

--- config_files/word_embeddings.py
+++ config_files/word_embeddings.py
@@ -6,6 +6,7 @@ from __future__ import division
 from __future__ import print_function
 
 import sys, io, os
+from dataset import word_embeddings
 
 DIR_WORD_EMBEDDINGS = "/home/sakae/Windows/public_model/embedding/"
 
@@ -29,9 +30,16 @@ cfg_word_embeddings = {
         "transform":None,
         "enable_phrase_composition":True
     },
-    "Word2Vec-umbc-sample": {
+    "General-word2vec-umbc-sample": {
         "path_numpy_array_binary_format":os.path.join(DIR_WORD_EMBEDDINGS, "word2vec-umbc-corpus-sample/w2v_cased_200_win5_sgns1.npy"),
         "path_vocabulary_text":os.path.join(DIR_WORD_EMBEDDINGS, "word2vec-umbc-corpus-sample/w2v_cased_200_win5_sgns1.vocab"),
+        "transform":None,
+        "enable_phrase_composition":True
+    },
+    "Word2Vec-encow16ax-cased": {
+        "path_word2vec_format":os.path.join(DIR_WORD_EMBEDDINGS, "word2vec-encow16ax-corpus/w2v_gensim_vector_cased_100_win5_min10_neg15_sgns1.bin"),
+        "mmap":"r",
+        "init_sims":False,
         "transform":None,
         "enable_phrase_composition":True
     }
