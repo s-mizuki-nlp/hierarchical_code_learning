f9418b8cbbf53b2c1cb0f6bf76f9857f57e56357
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2019-12-11 20:43:40 +0900

    ci: updated test execution script

--- exec_tests.sh
+++ exec_tests.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python -m unittest tests_inner.test_loss_layer
+python -m unittest tests_inner.test_loss_layer tests_inner.test_dataset_word_embeddings tests_inner.test_dataset_embeddings_plus_lexical_knowledge
