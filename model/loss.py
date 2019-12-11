25d380e6db2870d5c1509699adcf559bf4c91b19
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2019-12-11 22:02:57 +0900

    feat: implemented original mutual information loss

--- model/loss.py
+++ model/loss.py
@@ -52,7 +52,7 @@ class MutualInformationLoss(L._Loss):
 
     def entropy(self, probs: torch.Tensor) -> torch.Tensor:
         """
-        :param probs: probability array (variable shape)
+        :param probs: array of binary probability (variable shape)
         :return: ent = \sum_{x \in A} p(x)*lnp(x)
         """
 
@@ -85,6 +85,53 @@ class MutualInformationLoss(L._Loss):
     @property
     def scale(self):
         return self._scale
+
+
+class OriginalMutualInformationLoss(MutualInformationLoss):
+
+    def __init__(self, scale: float = 1.0, size_average=None, reduce=None, reduction='elementwise_mean'):
+        """
+        mutual information loss. it calculates the negative mutual information multiplied by the specified scale coefficient.
+        warning: it always apply a samplewise (=in-batch) mean. you can only choose elementwise mean or sum.
+        :return: scale * $-I(C;X)$ = scale * $-(\mean{H(C_n) - H(C_n|X)}$
+
+        :param scale:  scale coefficient
+        :param size_average:
+        :param reduce:
+        :param reduction:
+        """
+        super().__init__(scale, size_average, reduce, reduction)
+
+    def entropy(self, probs: torch.Tensor) -> torch.Tensor:
+        """
+        :param probs: array of multi-class probability with variable shape
+        :return: ent = \sum_{x_n \in A} p(x_n)*lnp(x_n) n_dim = probs.n_dim - 1
+        """
+
+        def _p_log_p(_probs):
+            return - _probs * torch.log(_probs+self._EPS)
+
+        ent = torch.sum(_p_log_p(probs), dim=-1)
+        return ent
+
+    def forward(self, t_prob_c: torch.Tensor):
+
+        # t_prob_c: (N_b, N_digits, N_ary); t_prob_c[b,n,c] = {p(c_n=c|x_b)}
+        # t_prob_c_zero_mean: (N_digits, N_ary)
+        t_prob_c_mean = torch.mean(t_prob_c, dim=0, keepdim=False)
+
+        # total_entropy: (N_digits,)
+        total_entropy = self.entropy(t_prob_c_mean)
+
+        # conditional_entropy: (N_digits,)
+        conditional_entropy = torch.mean(self.entropy(t_prob_c), dim=0)
+
+        if self.reduction in ("elementwise_mean", "mean"):
+            mutual_info = torch.mean(total_entropy - conditional_entropy)
+        elif self.reduction == "sum":
+            mutual_info = torch.sum(total_entropy - conditional_entropy)
+
+        return - self._scale * mutual_info
 
 
 ### supervised loss classes ###
