c784869ce9138f924a0624a18b7211b556298fe5
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2020-01-09 18:37:28 +0900

    feat: extended original encoder class so that it supports code probability adjustment method.

--- model/encoder.py
+++ model/encoder.py
@@ -12,6 +12,7 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 from .regressor import SoftmaxBasedCDFEstimator, ScheduledSoftmaxBasedCDFEstimator
+from .functional import CodeProbabiltyAdjuster
 from .encoder_internal import MultiDenseLayer, StackedLSTMLayer
 
 
@@ -52,7 +53,7 @@ class CodeLengthAwareEncoder(SimpleEncoder):
     def __init__(self, n_dim_emb: int, n_digits: int, n_ary: int,
                  n_dim_hidden: Optional[int] = None,
                  internal_layer_class: Optional[nn.Module] = None,
-                 use_scheduled_code_length_predictor: bool = False,
+                 code_length_predictor_class: Optional[nn.Module] = None,
                  dtype=torch.float32,
                  **kwargs_for_code_length_predictor):
 
@@ -63,8 +64,9 @@ class CodeLengthAwareEncoder(SimpleEncoder):
         self._n_digits = n_digits
         self._n_ary = n_ary
         self._dtype = dtype
-        self._use_scheduled_code_length_predictor = use_scheduled_code_length_predictor
+        self._code_length_predictor_class = code_length_predictor_class
         self._internal_layer_class = internal_layer_class
+        self._n_ary_internal = None
 
         self._build(**kwargs_for_code_length_predictor)
 
@@ -73,83 +75,27 @@ class CodeLengthAwareEncoder(SimpleEncoder):
         # x -> h: h = tanh(W*x+b)
         self.x_to_h = nn.Linear(in_features=self._n_dim_emb, out_features=self._n_dim_hidden)
 
-        # h -> z_n: z_n = softplus(f_n(h))
-        n_dim_h, n_dim_z = self._n_dim_hidden, self._n_ary-1
-        if (self._internal_layer_class is None) or (nn.Linear in inspect.getmro(self._internal_layer_class)):
-            lst_layers = [nn.Linear(in_features=n_dim_h, out_features=n_dim_z) for _ in range(self._n_digits)]
-        elif MultiDenseLayer in inspect.getmro(self._internal_layer_class):
-            lst_layers = []
-            for _ in range(self._n_digits):
-                l = MultiDenseLayer(n_dim_in=n_dim_h, n_dim_out=n_dim_z, n_dim_hidden=n_dim_h, n_layer=3, activation_function=F.relu,
-                                    bias=False)
-                lst_layers.append(l)
-        elif StackedLSTMLayer in inspect.getmro(self._internal_layer_class):
-            l = StackedLSTMLayer(n_dim_in=n_dim_h, n_dim_out=n_dim_z, n_dim_hidden=n_dim_h, n_layer=1, n_seq_len=self._n_digits)
-            lst_layers = [l]
+        # if adjuster class is specified, p(c_n) -> adjust so that p(c_n-1=0) <= p(c_n=0)
+        # if Estimator class is specified, x -> p(c_n=0): p(c_n=0) = \sum_{d=1 to n} g_d(x)
+        if (self._code_length_predictor_class is None) or \
+            (CodeProbabiltyAdjuster in inspect.getmro(self._code_length_predictor_class)):
+            self.code_length_predictor = CodeProbabiltyAdjuster.apply
+            self._n_ary_internal = self._n_ary
+        elif SoftmaxBasedCDFEstimator in inspect.getmro(self._code_length_predictor_class):
+            self.code_length_predictor = SoftmaxBasedCDFEstimator(n_dim_input=self._n_dim_emb, n_output=self._n_digits,
+                                                                  dtype=self._dtype,
+                                                                  **kwargs_for_code_length_predictor)
+            self._n_ary_internal = self._n_ary - 1
+        elif ScheduledSoftmaxBasedCDFEstimator in inspect.getmro(self._code_length_predictor_class):
+            self.code_length_predictor = ScheduledSoftmaxBasedCDFEstimator(n_dim_input=self._n_dim_emb, n_output=self._n_digits,
+                                                                           dtype=self._dtype,
+                                                                           **kwargs_for_code_length_predictor)
+            self._n_ary_internal = self._n_ary - 1
         else:
-            raise NotImplementedError(f"unsupported layer was specified: {self._internal_layer_class.__class__}")
-        self.lst_h_to_z_nonzero = nn.ModuleList(lst_layers)
-
-        # x -> p(c_n=0): p(c_n=0) = \sum_{d=1 to n} g_d(x)
-        if self._use_scheduled_code_length_predictor:
-            self.code_length_predictor = ScheduledSoftmaxBasedCDFEstimator(n_dim_input=self._n_dim_emb, n_output=self._n_digits, dtype=self._dtype,
-                                                              **kwargs_for_code_length_predictor)
-        else:
-            self.code_length_predictor = SoftmaxBasedCDFEstimator(n_dim_input=self._n_dim_emb, n_output=self._n_digits, dtype=self._dtype,
-                                                              **kwargs_for_code_length_predictor)
-
-    def forward(self, input_x: torch.Tensor):
-
-        # non-zero probability: p(c_n=k | x_b, c_n != 0)
-        t_h = torch.tanh(self.x_to_h(input_x))
-        if StackedLSTMLayer in inspect.getmro(self._internal_layer_class):
-            h_to_z = self.lst_h_to_z_nonzero[0]
-            # t_z: (n_batch, n_digits, n_ary-1)
-            t_z = torch.log(F.softplus(h_to_z(t_h)) + 1E-6)
-            # lst_z: [(n_batch, n_ary-1)]*n_digits
-            lst_z = list(map(torch.squeeze, t_z.split(1, dim=1)))
-        else:
-            lst_z = [torch.log(F.softplus(h_to_z(t_h)) + 1E-6) for h_to_z in self.lst_h_to_z_nonzero]
-        lst_prob_c_nonzero = [F.softmax(t_z, dim=-1) for t_z in lst_z]
-
-        # t_prob_c_nonzero: (n_batch, n_digits, N_ary-1)
-        t_prob_c_nonzero = torch.stack(lst_prob_c_nonzero, dim=1)
-
-        # zero probability: p(c_n=0 | x_b)
-        # t_prob_c_zero: (N_batch, N_digits) -> (N_batch, N_digits, 1)
-        t_prob_c_zero = self.code_length_predictor.forward(input_x)
-        t_prob_c_zero = t_prob_c_zero.unsqueeze(-1)
-
-        # t_prob_c_zero: (N_batch, N_digits, N_ary)
-        t_prob_c = torch.cat((t_prob_c_zero, (1.0-t_prob_c_zero)*t_prob_c_nonzero), dim=-1)
-
-        return t_prob_c
-
-    @property
-    def gate_open_ratio(self):
-        return getattr(self.code_length_predictor, "gate_open_ratio", None)
-
-    @gate_open_ratio.setter
-    def gate_open_ratio(self, value):
-        if self.gate_open_ratio is not None:
-            setattr(self.code_length_predictor, "gate_open_ratio", value)
-
-
-class IntegratedCodeLengthAwareEncoder(CodeLengthAwareEncoder):
-
-    def _dtype_and_device(self, t: torch.Tensor):
-        return t.dtype, t.device
-
-    def _build(self, **kwargs_for_code_length_predictor):
-
-        if len(kwargs_for_code_length_predictor) > 0:
-            warnings.warn("this class does not use independent code length predictor.")
-
-        # x -> h: h = tanh(W*x+b)
-        self.x_to_h = nn.Linear(in_features=self._n_dim_emb, out_features=self._n_dim_hidden)
+            raise NotImplementedError(f"unsupported layer was specified: {self._code_length_predictor_class.__class__}")
 
         # h -> z_n: z_n = softplus(f_n(h))
-        n_dim_h, n_dim_z = self._n_dim_hidden, self._n_ary
+        n_dim_h, n_dim_z = self._n_dim_hidden, self._n_ary_internal
         if (self._internal_layer_class is None) or (nn.Linear in inspect.getmro(self._internal_layer_class)):
             lst_layers = [nn.Linear(in_features=n_dim_h, out_features=n_dim_z) for _ in range(self._n_digits)]
         elif MultiDenseLayer in inspect.getmro(self._internal_layer_class):
@@ -165,52 +111,45 @@ class IntegratedCodeLengthAwareEncoder(CodeLengthAwareEncoder):
             raise NotImplementedError(f"unsupported layer was specified: {self._internal_layer_class.__class__}")
         self.lst_h_to_z = nn.ModuleList(lst_layers)
 
-    def _adjust_code_probability(self, t_prob_c: torch.Tensor):
-        dtype, device = self._dtype_and_device(t_prob_c)
-        n_batch = t_prob_c.shape[0]
-
-        # t_prob_zero_prev: (n_batch,)
-        t_prob_zero_prev = torch.zeros((n_batch,), dtype=dtype, device=device)
-        lst_t_prob_c_zero_adj = []
-        for digit in range(self._n_digits):
-            # t_prob_c_zero_n: (n_batch,)
-            t_prob_c_zero_n = t_prob_c[:,digit,0]
-            t_prob_c_zero_adj_n = t_prob_zero_prev + t_prob_c_zero_n*(1. - t_prob_zero_prev)
-            lst_t_prob_c_zero_adj.append(t_prob_c_zero_adj_n)
-            t_prob_zero_prev = t_prob_c_zero_adj_n
-
-        # t_prob_c_zero_adj: (n_batch, n_digits, 1)
-        t_prob_c_zero_adj = torch.stack(lst_t_prob_c_zero_adj, dim=1).unsqueeze(-1)
-
-        # t_prob_c_nonzero: (n_batch, n_digits, n_ary-1)
-        t_prob_c_nonzero = torch.narrow(t_prob_c, dim=-1, start=1, length=self._n_digits-1) + 1E-6
-        # coef_adj: (n_batch, n_digits, 1)
-        coef_adj = (1.-t_prob_c_zero_adj) / torch.sum(t_prob_c_nonzero, dim=-1, keepdim=True)
-        # t_prob_c_nonzero_adj: (n_batch, n_digits, n_ary-1)
-        t_prob_c_nonzero_adj = coef_adj * t_prob_c_nonzero
-
-        # t_prob_c_adj: (n_batch, n_digits, n_ary)
-        t_prob_c_adj = torch.cat((t_prob_c_zero_adj, t_prob_c_nonzero_adj), dim=-1)
-
-        return t_prob_c_adj
-
     def forward(self, input_x: torch.Tensor):
-        # original probability: p(c_n=k | x_b)
+
+        # code probability: p(c_n=m)
         t_h = torch.tanh(self.x_to_h(input_x))
         if StackedLSTMLayer in inspect.getmro(self._internal_layer_class):
             h_to_z = self.lst_h_to_z[0]
-            # t_z: (n_batch, n_digits, n_ary)
+            # t_z: (n_batch, n_digits, n_ary_internal)
             t_z = torch.log(F.softplus(h_to_z(t_h)) + 1E-6)
-            # lst_z: [(n_batch, n_ary)]*n_digits
+            # lst_z: [(n_batch, n_ary_internal)]*n_digits
             lst_z = list(map(torch.squeeze, t_z.split(1, dim=1)))
         else:
             lst_z = [torch.log(F.softplus(h_to_z(t_h)) + 1E-6) for h_to_z in self.lst_h_to_z]
         lst_prob_c = [F.softmax(t_z, dim=-1) for t_z in lst_z]
 
-        # t_prob_c: (n_batch, n_digits, n_ary)
+        # t_prob_c: (n_batch, n_digits, n_ary_internal)
         t_prob_c = torch.stack(lst_prob_c, dim=1)
 
-        # adjust zero probability: p(c_n=0) = p(c_n-1=0) + p(c_n=0)*p(c_n-1 != 0)
-        t_prob_c_adj = self._adjust_code_probability(t_prob_c)
+        if self._n_ary_internal == self._n_ary - 1:
+            # predict zero probability: p(c_n=0) separately using code length predictor
+            # t_prob_c_zero: (n_batch, n_digits) -> (n_batch, n_digits, 1)
+            t_prob_c_zero = self.code_length_predictor.forward(input_x)
+            t_prob_c_zero = t_prob_c_zero.unsqueeze(-1)
 
-        return t_prob_c_adj
+            # concat it with code probability
+            # t_prob_c: (n_batch, n_digits, n_ary)
+            t_prob_c = torch.cat((t_prob_c_zero, (1.0-t_prob_c_zero)*t_prob_c), dim=-1)
+        elif self._n_ary_internal == self._n_ary:
+            # otherwise, simply adjust code probability so that it satisfies monotone increasing condition.
+            t_prob_c = self.code_length_predictor(t_prob_c)
+        else:
+            raise ValueError(f"unexpected internal value: {self._n_ary_internal}")
+
+        return t_prob_c
+
+    @property
+    def gate_open_ratio(self):
+        return getattr(self.code_length_predictor, "gate_open_ratio", None)
+
+    @gate_open_ratio.setter
+    def gate_open_ratio(self, value):
+        if self.gate_open_ratio is not None:
+            setattr(self.code_length_predictor, "gate_open_ratio", value)
