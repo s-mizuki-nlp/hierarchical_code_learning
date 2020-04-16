cb44c63f10837f47d71bedbb82e75e981073fd28
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2020-04-16 21:15:25 +0900

    ci: split normalized / unnnormalized fasttext embeddings

--- config_files/word_embeddings.py
+++ config_files/word_embeddings.py
@@ -18,11 +18,17 @@ cfg_word_embeddings = {
         "transform":None,
         "enable_phrase_composition":True
     },
-    "fastText-wiki-news": {
+    "fastText-wiki-news-norm": {
         "path_fasttext_binary_format":os.path.join(DIR_WORD_EMBEDDINGS, "fasttext-wiki-news-300/wiki-news-300d-1M-subword.bin"),
         "transform":None,
         "enable_phrase_composition":True,
         "norm":True
+    },
+    "fastText-wiki-news": {
+        "path_fasttext_binary_format":os.path.join(DIR_WORD_EMBEDDINGS, "fasttext-wiki-news-300/wiki-news-300d-1M-subword.bin"),
+        "transform":None,
+        "enable_phrase_composition":True,
+        "norm":False
     },
     "Word2Vec-GloVe-wiki-gigaword": {
         "path_word2vec_format":os.path.join(DIR_WORD_EMBEDDINGS, "glove-wiki-gigaword-200/glove-wiki-gigaword-200.gz"),
