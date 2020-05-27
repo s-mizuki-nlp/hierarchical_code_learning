b248336ef95af8cdb742453d1c038a6509049488
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2020-05-28 02:49:08 +0900

    feat: implemented non-zero probability adjusted simple decoder.

--- model/decoder.py
+++ model/decoder.py
@@ -6,7 +6,6 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
-
 class SimpleDecoder(nn.Module):
 
     def __init__(self, n_dim_emb: int, n_digits: int, n_ary: int, **kwargs):
@@ -24,15 +23,37 @@ class SimpleDecoder(nn.Module):
         return self._n_ary
 
     def _build(self):
-        self.lst_c_to_x = nn.ModuleList([nn.Linear(in_features=self._n_ary, out_features=self._n_dim_emb, bias=False) for n in range(self._n_digits)])
+        self.lst_c_to_x = nn.ModuleList([nn.Linear(in_features=self._n_ary, out_features=self._n_dim_emb, bias=False) for d in range(self._n_digits)])
 
     def forward(self, input_c: torch.Tensor):
 
         # input_c: (N_b, N_digits, N_ary)
         lst_x = []
-        for n in range(self._n_digits):
+        for d in range(self._n_digits):
             # lst_x[n]: (N_b, N_dim_emb)
-            lst_x.append(self.lst_c_to_x[n](input_c[:,n,:]))
+            lst_x.append(self.lst_c_to_x[d](input_c[:,d,:]))
+
+        # t_x_dash: \sum_n{(N_b, n, N_dim_emb)} = (N_b, N_dim_emb)
+        t_x_dash = torch.sum(torch.stack(lst_x, dim=1), dim=1)
+
+        return t_x_dash
+
+
+class ProbAdjustedSimpleDecoder(SimpleDecoder):
+
+    def forward(self, prob_c: torch.Tensor):
+
+        # input_c: (N_b, N_digits, N_ary)
+        lst_x = []
+        n_batch = prob_c.shape[0]
+        prob_nonzero_prev = torch.ones((n_batch, 1), dtype=torch.float32, device=prob_c.device, requires_grad=False)
+        for d in range(self._n_digits):
+            # probs: p(c_d = a, c_d-1 != 0) = p(c_d = a | c_d-1 != 0)*p(c_d-1 != 0)
+            probs = prob_c[:, d, :] * prob_nonzero_prev
+            # lst_x[n]: (N_b, N_dim_emb)
+            lst_x.append(self.lst_c_to_x[d](probs))
+            # update nonzero probability
+            prob_nonzero_prev = prob_nonzero_prev * torch.sum(prob_c[:, d, 1:], dim=-1, keepdim=True)
 
         # t_x_dash: \sum_n{(N_b, n, N_dim_emb)} = (N_b, N_dim_emb)
         t_x_dash = torch.sum(torch.stack(lst_x, dim=1), dim=1)
