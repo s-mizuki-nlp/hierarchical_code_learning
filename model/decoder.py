f0dfe1656268c44c787928a2f437a14f73cd8075
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2020-05-28 03:07:39 +0900

    feat: implemented digit-wise weight adjustment feature

--- model/decoder.py
+++ model/decoder.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-from typing import List, Optional
+from typing import List, Optional, Callable
 import torch
 from torch import nn
 from torch.nn import functional as F
 
 class SimpleDecoder(nn.Module):
 
-    def __init__(self, n_dim_emb: int, n_digits: int, n_ary: int, **kwargs):
+    def __init__(self, n_dim_emb: int, n_digits: int, n_ary: int, digit_weight_function: Optional[Callable[[int],float]] = None, **kwargs):
 
         super(SimpleDecoder, self).__init__()
 
@@ -16,14 +16,17 @@ class SimpleDecoder(nn.Module):
         self._n_digits = n_digits
         self._n_ary = n_ary
 
-        self._build()
+        if digit_weight_function is None:
+            digit_weight_function = lambda d: 1.0
+        self._build(digit_weight_function = digit_weight_function)
 
     @property
     def n_ary(self):
         return self._n_ary
 
-    def _build(self):
+    def _build(self, digit_weight_function: Callable[[int],float]):
         self.lst_c_to_x = nn.ModuleList([nn.Linear(in_features=self._n_ary, out_features=self._n_dim_emb, bias=False) for d in range(self._n_digits)])
+        self.digit_weights = [digit_weight_function(d+1) for d in range(self._n_digits)]
 
     def forward(self, input_c: torch.Tensor):
 
@@ -31,7 +34,8 @@ class SimpleDecoder(nn.Module):
         lst_x = []
         for d in range(self._n_digits):
             # lst_x[n]: (N_b, N_dim_emb)
-            lst_x.append(self.lst_c_to_x[d](input_c[:,d,:]))
+            weight_d = self.digit_weights[d]
+            lst_x.append(weight_d * self.lst_c_to_x[d](input_c[:,d,:]))
 
         # t_x_dash: \sum_n{(N_b, n, N_dim_emb)} = (N_b, N_dim_emb)
         t_x_dash = torch.sum(torch.stack(lst_x, dim=1), dim=1)
@@ -51,7 +55,9 @@ class ProbAdjustedSimpleDecoder(SimpleDecoder):
             # probs: p(c_d = a, c_d-1 != 0) = p(c_d = a | c_d-1 != 0)*p(c_d-1 != 0)
             probs = prob_c[:, d, :] * prob_nonzero_prev
             # lst_x[n]: (N_b, N_dim_emb)
-            lst_x.append(self.lst_c_to_x[d](probs))
+            # weight_d = digit_weight_function(d)
+            weight_d = self.digit_weights[d]
+            lst_x.append(weight_d * self.lst_c_to_x[d](probs))
             # update nonzero probability
             prob_nonzero_prev = prob_nonzero_prev * torch.sum(prob_c[:, d, 1:], dim=-1, keepdim=True)
 
