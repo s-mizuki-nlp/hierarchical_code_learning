8a216668591e5feb64427523b0a3bf686a733ad0
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2020-08-26 19:53:04 +0900

    feat: enabled string-type query instead of vector-type query.

--- dataset/utils.py
+++ dataset/utils.py
@@ -31,8 +31,18 @@ class EmbeddingSimilaritySearch(object):
     def n_dim(self):
         return self._embeddings.shape[1]
 
-    def most_similar(self, vector: np.array, top_k: Optional[int] = None, top_q: Optional[float] = None, excludes: Optional[Iterable[str]] = None) -> List[Tuple[str, float]]:
+    def most_similar(self, entity: Optional[str] = None, vector: Optional[np.array] = None,
+                     top_k: Optional[int] = None, top_q: Optional[float] = None, excludes: Optional[Iterable[str]] = None) -> List[Tuple[str, float]]:
+
+        assert (entity is not None) or (vector is not None), "you must specify either `entity` or `vector` argument."
         assert (top_k is not None) or (top_q is not None), "you must specify either `top_k` or `top_q` argument."
+
+        if entity is not None:
+            assert entity in self._entity2idx, f"'{entity}' is not found."
+            vector = self._embeddings[self._entity2idx[entity]]
+            remove_query_entity = True
+        else:
+            remove_query_entity = False
 
         if top_q is not None:
             top_k = math.ceil(self.n_vocab * top_q)
@@ -50,13 +60,16 @@ class EmbeddingSimilaritySearch(object):
             idx2entity = OrderedDict(enumerate(entities))
             embeddings = self._embeddings[indices,:]
 
-        return self._most_similar_topk(vector, embeddings, idx2entity, top_k)
+        return self._most_similar_topk(vector, embeddings, idx2entity, top_k, remove_query_entity)
 
-    def _most_similar_topk(self, vector: np.array, embeddings: np.ndarray, idx2entity: Dict[int, str], top_k: int) -> List[Tuple[str, float]]:
+    def _most_similar_topk(self, vector: np.array, embeddings: np.ndarray, idx2entity: Dict[int, str], top_k: int, remove_query_entity: bool = False) -> List[Tuple[str, float]]:
         vector = vector / (np.linalg.norm(vector, ord=2) + self.__EPS)
 
         vec_similarity = embeddings.dot(vector)
-        vec_indices_topk = np.argsort(-vec_similarity)[:top_k]
+        if remove_query_entity:
+            vec_indices_topk = np.argsort(-vec_similarity)[1:top_k+1]
+        else:
+            vec_indices_topk = np.argsort(-vec_similarity)[:top_k]
 
         lst_ret = [(idx2entity[idx], vec_similarity[idx]) for idx in vec_indices_topk]
         return lst_ret
