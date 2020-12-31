02212d024dd75c3b65356d288f6a6b63302711d2
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2020-12-31 17:25:43 +0900

    ci: registered another lexical knowledge dataset which excludes xBLESS relation only.

--- config_files/lexical_knowledge_datasets_nguyen.py
+++ config_files/lexical_knowledge_datasets_nguyen.py
@@ -12,6 +12,7 @@ from dataset.filter import DictionaryFilter
 from .wordnet_constant import SYNSETS_DEPTH_UP_TO_SECOND_LEVEL
 _distance_str_to_float = FieldTypeConverter(dict_field_type_converter={"distance":np.float32})
 _exclude_top_synsets = DictionaryFilter(excludes={"synset_hypernym":set(SYNSETS_DEPTH_UP_TO_SECOND_LEVEL)})
+_exclude_synonymy_relations = DictionaryFilter(excludes={"distance":{0,"0"}})
 
 DIR_LEXICAL_KNOWLEDGE = "/home/sakae/Windows/dataset/hypernym_detection/wordnet_nguyen_2017/"
 DIR_LEXICAL_KNOWLEDGE_VER2 = "/home/sakae/Windows/dataset/hypernym_detection/wordnet_nguyen_2017_ver2/"
@@ -68,6 +69,16 @@ cfg_hyponymy_relation_datasets = {
         "columns": {"hyponym":0, "hypernym":1, "distance":2, "pos":3, "synset_hyponym":4, "synset_hypernym":5},
         "transform": _distance_str_to_float,
         "description": "WordNet hyponymy and synonymy relation dataset: noun and verb",
+    },
+    "WordNet-hyponymy-noun-verb-ver2": {
+        "path": os.path.join(DIR_LEXICAL_KNOWLEDGE_VER2, "lexical_knowledge_wordnet_hyponymy_synonymy_noun_verb_valid_case_sensitive.txt"),
+        "header": True,
+        "delimiter": "\t",
+        "columns": {"hyponym":0, "hypernym":1, "distance":2, "pos":3, "synset_hyponym":4, "synset_hypernym":5},
+        "transform": _distance_str_to_float,
+        "filter": _exclude_synonymy_relations,
+        "description": "WordNet hyponymy relation dataset ver2(=excludes duplicate in xBLESS relation): noun and verb",
     }
+
 }
 
