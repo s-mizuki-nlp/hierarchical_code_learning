dcc2ca811274baffa58272b68edc10e3110c82a5
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2021-05-20 20:05:54 +0900

    feat(added function filter constructor class.):

--- dataset/filter.py
+++ dataset/filter.py
@@ -6,7 +6,7 @@ from __future__ import division
 from __future__ import print_function
 
 
-from typing import Optional, Dict, Set
+from typing import Optional, Dict, Set, Callable, Any
 
 
 class DictionaryFilter(object):
@@ -31,3 +31,27 @@ class DictionaryFilter(object):
                 if sample[field_name] in values:
                     return False
             return True
+
+
+class FunctionFilter(object):
+
+    def __init__(self, include: Optional[Callable[[Dict[str, str]], bool]] = None,
+                 exclude: Optional[Callable[[Dict[str, str]], bool]] = None):
+        self._include = include
+        self._exclude = exclude
+        assert (include is not None) or (exclude is not None), f"you must specify either `includes` or `excludes` argument."
+        assert (include is None) or (exclude is None), f"you can't specify both `includes` and `excludes` at the same time."
+
+    def __call__(self, sample: Dict[str, str]):
+
+        if self._include is not None:
+            if self._include(sample) == True:
+                return True
+            else:
+                return False
+
+        if self._exclude is not None:
+            if self._exclude(sample) == True:
+                return False
+            else:
+                return True
